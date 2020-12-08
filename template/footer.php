586300f35b9645c612fa635b75b8f5587d5e5e42
lzampieri <zampieri.leonardo98@gmail.com>
2020-12-08 19:33:49 +0100

    Connected shinystats and easy_cms

--- template/footer.php
+++ template/footer.php
@@ -1,2 +1,10 @@
+<!-- Inizio Codice ShinyStat -->
+<script type="text/javascript" src="//codice.shinystat.com/cgi-bin/getcod.cgi?USER=galielo"></script>
+<noscript>
+<a href="//www.shinystat.com/it/" target="_top">
+<img src="//www.shinystat.com/cgi-bin/shinystat.cgi?USER=galielo" alt="Contatore per siti" style="border:0px" /></a>
+</noscript>
+<!-- Fine Codice ShinyStat -->
+
 </body>
 </html>
